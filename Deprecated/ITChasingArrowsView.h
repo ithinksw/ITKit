3dbcf6ca4d5e5e9f192b8238a82fbcc6e8099d6f
Matthew Judy <mjudy@ithinksw.com>
2003-01-27 09:16:48 +0000

    ITKit for Menutunes Commit Phase 3: Adding the Deprecated Directory and Files
    This checkin moves the ITChasingArrows stuff to the new Deprecated directory.

---
+++ Deprecated/ITChasingArrowsView.h
@@ -0,0 +1,32 @@
+/*
+ *	ITKit
+ *  ITChasingArrowsView
+ *    Animating Asynchronous Arrows Widget
+ *    *** DEPRECATED: NSProgressIndicator now offers an async widget.  MLJ - 01/14/2003
+ *
+ *  Original Author : Doug Brown <...>
+ *   Responsibility : Matt Judy <mjudy@ithinksw.com>
+ *   Responsibility : Joseph Spiros <joseph.spiros@ithinksw.com>
+ *
+ *  Copyright (c) 2002 - 2003 iThink Software.
+ *  All Rights Reserved
+ *
+ */
+
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ITChasingArrowsView : NSView
+{
+    BOOL running, inForeground;
+    int curIndex;
+    NSTimer *timer;
+    NSArray *images;
+}
+
+- (IBAction)stop:(id)sender;
+- (IBAction)start:(id)sender;
+
+
+@end
